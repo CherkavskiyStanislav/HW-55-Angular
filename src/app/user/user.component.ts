b90e5bcb81c82838092162494afd4dde69acfd3d
Cherkavskiy Stanislav <cherkavskiy.stas@gmail.com>
2023-04-14 19:25:16 +0300

    Completed homework 55

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, Output } from '@angular/core';
+import { UsersService } from '../service/users.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+  @Input() name = ''
+  @Input() status = ''
+  @Input() img = ''
+  constructor(
+    public usersService: UsersService
+  ) {}
+
+  changeName(value: string) {
+    this.name = value;
+  }
+  changeStatus(value: any) {
+    this.status = value;
+  }
+}
